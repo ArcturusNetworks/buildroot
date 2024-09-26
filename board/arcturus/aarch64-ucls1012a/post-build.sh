aa2d9c4668fb41dbbaf0616965ee0f82bb9739ea
Oleksandr G Zhadan <Oleks@ArcturusNetworks.com>
2024-09-26 15:38:25 -0400

    Disable local HG repository u-boot build
    Signed-off-by: Oleksandr G Zhadan <Oleks@ArcturusNetworks.com>

--- board/arcturus/aarch64-ucls1012a/post-build.sh
+++ board/arcturus/aarch64-ucls1012a/post-build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 mkdir -p ${TARGET_DIR}/home
-cp -f ${BUILD_DIR}/uboot-custom/u-boot-rcw-pfe.bin ${BINARIES_DIR}/u-boot-firmware.bin
+cp -f ${BUILD_DIR}/uboot-$3/u-boot-rcw-pfe.bin ${BINARIES_DIR}/u-boot-firmware.bin
+cp -f ${BUILD_DIR}/uboot-$3/u-boot.bin ${BINARIES_DIR}/u-boot.bin
 cp -f ${BUILD_DIR}/linux-custom/$2 ${BINARIES_DIR}/
